319176620df9b30c543740b7315a6e83e52f8450
ka <insideka01@gmail.com>
2021-08-21 00:43:38 +0900

    ch05 연습문제 1번

---
+++ dockc/ch05-1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int nResult, w = 0, x = -1, y = -1, z = 1;
+    nResult = w++ || x++ && ++y || ++z;
+    printf("%d %d %d %d %d\n", w, x, y, z, nResult);
+    return 0;
+}
