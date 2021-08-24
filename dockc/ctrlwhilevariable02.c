cff9ce7405111bb23d88d1fd951c10cfb52d38e6
ka <insideka01@gmail.com>
2021-08-24 11:04:28 +0900

    while 반복문 안에 변수선언

---
+++ dockc/ctrlwhilevariable02.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char ch = 0;
+
+    while ((ch = getchar()) != '\n')
+    {
+        int nIndex = 0;
+        printf("%02d\t%c\n", nIndex, ch);
+        ++nIndex;
+    }
+
+    return 0;
+}
