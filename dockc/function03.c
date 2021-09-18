8809ec60f2f641586c52936d297a7c31d38b49e4
ka <insideka01@gmail.com>
2021-09-19 00:47:37 +0900

    피호출자 함수의 반환값의 즉시 활용 세가지

---
+++ dockc/function03.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int GetMax(int a, int b, int c)
+{
+    int nMax = a;
+    if(b > nMax) nMax = b;
+    if(c > nMax) nMax = c;
+
+    return nMax;
+}
+
+int main(void)
+{
+    int nResult = 0;
+
+    printf("MAX: %d\n", GetMax(1, 2, 3));
+
+    printf("MAX: %d\n", GetMax(2, 3, 1) * 2);
+
+    printf("MAX: %d\n", nResult = GetMax(3, 2, 1));
+
+    return 0;
+}
