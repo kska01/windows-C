3b473588c3d09f8e2804a35efca7fd8ddfc6d5ba
ka <insideka01@gmail.com>
2021-09-21 14:16:44 +0900

    전역변수 사용의 안좋은 예

---
+++ dockc/codedevbad01.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int g_aList[5] = {0};
+
+void InitList(void)
+{
+    int i = 0;
+    for(i = 0; i < 5; ++i)
+    {
+        printf("정수를 입력하세요. : ");
+        scanf("%d", &g_aList[i]);
+    }
+}
+
+void SortList(void)
+{
+    int i = 0, j = 0, nTmp = 0;
+
+    for(i = 0; i < 4; ++i)
+        for(j = i + 1; j < 5; ++j)
+            if(g_aList[j] < g_aList[i])
+            {
+                nTmp = g_aList[j];
+                g_aList[j] = g_aList[i];
+                g_aList[i] = nTmp;
+            }
+}
+ 
+void PrintList(void)
+{
+    int i = 0;
+    for(i = 0; i < 5; ++i)
+        printf("%d\t", g_aList[i]);
+    putchar('\n');
+}
+
+int main(void)
+{
+    InitList();
+    SortList();
+    PrintList();
+    return 0;
+}
